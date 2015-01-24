3a2bef4a99807ed5081d0dafe0131e7e0962faa7
Shou Ya <shouyatf@gmail.com>
2015-01-24 01:02:50 -0600

    type->string, instance Show Type.

--- nya.rkt
+++ nya.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 
 (require racket/match)
+(require racket/string)
 
 '((deftype (T/Nil  :: a))
   (deftype (T/Cons :: a -> (T/U Nil (Cons a)) -> (Cons a)))
@@ -30,7 +31,8 @@
     [(list 'T/U ts ...)    (make-type-union ts)]
     [(list a)              (compile-type a)]
     [(list f '-> r ...)    (make-func-type
-                            f (compile-type r))]
+                            (compile-type f)
+                            (compile-type r))]
     [(list tf ts ...)      (make-typefunc-type
                             tf (map compile-type ts))]
     [(? global-type-name?) (make-global-type type)]
@@ -58,7 +60,6 @@
 (define (make-typefunc-type tf ts)
   (cons 'TF (cons tf ts)))
 
-
 (define (type-of expr [ns '()])
   (match expr
     [(list f   args ...) (type-of-func-appl f args ns)]
@@ -78,20 +79,47 @@
 
 (define (type-of-func-appl func args ns)
   (match (get-func-type func ns)
-    [('() (error 'type-of-func-appl "function '~a' not found" func))]
+    ['() (error 'type-of-func-appl "function '~a' not found" func)]
     [(cons 'FT (cons at rett)) ;; a -> b
      (match args
        [(cons a rst) (if (is-of-type? a at ns)
                          'you-re-cool-and-preceed-the-rest   ; TODO: undefined
-                         (fc-fail-unmatched (type-of a) at))]
-       [('())        rett])]
-    ))
+                         (tc-fail-unmatched (type-of a) at))]
+       ['()          rett])]
+    )) ; TODO: another case, pure value vs. empty args
 
 
 (define (is-of-type? val type [ns '()])
   (type< (type-of val ns) type))
 
 (define (type< t1 t2)
-  undefined)
+  undefined)  ; TODO: undefined
+
+(define (get-func-type func-name ns)
+  undefined) ; TODO: undefined
+
+(define undefined '())
+
+
+; U T TV F TF
+(define (type->string type)
+  (match type
+    [(cons 'T t)     (symbol->string t)]
+    [(cons 'TV v)    (symbol->string v)]
+    [(cons 'U xs)    (string-join (map type->string xs) " + "
+                                  #:before-first "("
+                                  #:after-last ")")]
+    [(cons 'F (cons a r))
+     (string-append (type->string a)
+                    " -> "
+                    (type->string r))]
+    [(cons 'TF (cons t ts))
+     (string-join (cons (symbol->string t)
+                        (map type->string ts))
+                  " "
+                  #:before-first "("
+                  #:after-last ")")]
+    ))
 
 (compile-type '(A -> (B a)))
+(type->string (compile-type '(A -> (B a))))
